cbb2fd96411fb3ec00dc98228c67ae21f3014023
vishwasjadhav95 <vishwas.jadhav@netcentric.biz>
2025-02-06 16:11:39 +0530

    Error resolved

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,11 +1,9 @@
 export default function decorate(block) {
     const classNames = ['cmp-teaser-content', 'cmp-teaser-image', 'cmp-teaser-details'];
-  
     [...block.children].forEach((row, rowIndex) => {
       if (rowIndex === 0) {
         row.classList.add(classNames[0]);
       }
-  
       [...row.children].forEach((col, colIndex) => {
         if (colIndex === 0) {
           col.classList.add(classNames[1]);
