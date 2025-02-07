50a2a70efce8a339cee68a68883f5752ad2bdcf5
vishwasjadhav95 <vishwas.jadhav@netcentric.biz>
2025-02-07 10:57:28 +0530

    error resolved

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,17 +1,16 @@
 export default function decorate(block) {
-    const rightTeaserBlock = document.querySelector('.teaser.cmp-right-teaser.block');
-    console.log(rightTeaserBlock);
-    const classNames = ['cmp-teaser-content', 'cmp-teaser-image', 'cmp-teaser-details'];
-    [...block.children].forEach((row, rowIndex) => {
-      if (rowIndex === 0) {
-        row.classList.add(classNames[0]);
+  const classNames = ['cmp-teaser-content', 'cmp-teaser-image', 'cmp-teaser-details'];
+  [...block.children].forEach((row, rowIndex) => {
+    if (rowIndex === 0) {
+      row.classList.add(classNames[0]);
+    }
+    [...row.children].forEach((col) => {
+      if (col.querySelector('picture')) {
+        col.classList.add(classNames[1]);
+      } else {
+        col.classList.add(classNames[2]);
       }
-      [...row.children].forEach((col) => {
-        if (col.querySelector('picture')) {
-          col.classList.add(classNames[1]);
-        } else {
-          col.classList.add(classNames[2]);
-        }
       });
     });
   }
+  
