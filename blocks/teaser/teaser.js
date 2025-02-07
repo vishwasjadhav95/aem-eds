d040e127d7c435ff9689e6072506864b2a8ed104
vishwasjadhav95 <vishwas.jadhav@netcentric.biz>
2025-02-07 10:51:00 +0530

    files updated

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,15 +1,17 @@
 export default function decorate(block) {
-  const classNames = ['cmp-teaser-content', 'cmp-teaser-image', 'cmp-teaser-details'];
-  [...block.children].forEach((row, rowIndex) => {
-    if (rowIndex === 0) {
-      row.classList.add(classNames[0]);
-    }
-    [...row.children].forEach((col, colIndex) => {
-      if (colIndex === 0) {
-        col.classList.add(classNames[1]);
-      } else if (colIndex === 1) {
-        col.classList.add(classNames[2]);
+    const rightTeaserBlock = document.querySelector('.teaser.cmp-right-teaser.block');
+    console.log(rightTeaserBlock);
+    const classNames = ['cmp-teaser-content', 'cmp-teaser-image', 'cmp-teaser-details'];
+    [...block.children].forEach((row, rowIndex) => {
+      if (rowIndex === 0) {
+        row.classList.add(classNames[0]);
       }
+      [...row.children].forEach((col) => {
+        if (col.querySelector('picture')) {
+          col.classList.add(classNames[1]);
+        } else {
+          col.classList.add(classNames[2]);
+        }
+      });
     });
-  });
-}
+  }
